6007578263434eed5564dbd105e9359944c21aad
Haoran Qi <Haoran_Qi@outlook.com>
2020-09-08 15:46:45 +0800

    amd64 only

--- amd64/v2ray-plugin.sh
+++ amd64/v2ray-plugin.sh
@@ -10,36 +10,7 @@
 # https://github.com/shadowsocks/v2ray-plugin
 
 PLATFORM=$1
-if [ -z "$PLATFORM" ]; then
-    ARCH="amd64"
-else
-    case "$PLATFORM" in
-        linux/386)
-            ARCH="386"
-            ;;
-        linux/amd64)
-            ARCH="amd64"
-            ;;
-        linux/arm/v6)
-            ARCH="arm6"
-            ;;
-        linux/arm/v7)
-            ARCH="arm7"
-            ;;
-        linux/arm64|linux/arm64/v8)
-            ARCH="arm64"
-            ;;
-        linux/ppc64le)
-            ARCH="ppc64le"
-            ;;
-        linux/s390x)
-            ARCH="s390x"
-            ;;
-        *)
-            ARCH=""
-            ;;
-    esac
-fi
+ARCH="amd64"
 [ -z "${ARCH}" ] && echo "Error: Not supported OS Architecture" && exit 1
 # Download v2ray-plugin binary file
 V2RAY_PLUGIN_FILE="v2ray-plugin_linux_${ARCH}"
